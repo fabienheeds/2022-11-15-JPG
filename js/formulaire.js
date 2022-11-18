9015f861892203fa1ea30cfd0f88ee8e36183698
fabien <fabien@heeds.eu>
2022-11-18 08:51:41 +0100

    POO Meme

---
+++ js/formulaire.js
@@ -0,0 +1,28 @@
+class VueFormulaire
+{
+    // champ privé
+    #vueHref = 'create.html';
+    domRefElement = undefined;
+    meme = undefined;
+    constructor()
+    {
+
+    }
+    /**
+     * Fonction privée
+     */
+    #addEvents = () =>
+    {
+
+    }
+    loadPage = (domRefWrapper) =>
+    {
+
+    }
+    changeMemeValue = (partialMemeData, evt) => {
+        
+    }
+    onsSubmitForm = (evt) =>{
+        
+    }
+}
